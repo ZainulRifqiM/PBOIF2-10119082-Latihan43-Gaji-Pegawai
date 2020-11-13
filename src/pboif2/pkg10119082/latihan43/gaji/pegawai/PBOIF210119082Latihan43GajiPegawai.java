2d6d78cd62a64864b75a8acfdcc92b24b4a2a06b
Zainul Rifqi <Zainul Rifqi@LAPTOP-O1E2M14H>
2020-11-13 10:07:22 +0700

    PBOIF2-10119082-Latihan43-Gaji Pegawai

---
+++ src/pboif2/pkg10119082/latihan43/gaji/pegawai/PBOIF210119082Latihan43GajiPegawai.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119082.latihan43.gaji.pegawai;
+
+/**
+ *
+ * @author Zainul Rifqi
+ * NAMA     : Zainul Rifqi Muwaffaq
+ * KELAS    : IF2
+ * NIM      : 10119082
+ * Deskripsi: Gaji Pegawai
+ */
+public class PBOIF210119082Latihan43GajiPegawai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        GajiPegawai gaji = new GajiPegawai();
+        System.out.println("###Data Gaji Karyawan PT. KAKATU###");
+        System.out.println("-----------------------------------");
+        
+        gaji.setNama("Rizki Adam Kurniawan");
+        gaji.setAlamat("Jalan Semar dlm 4 No 72/66");
+        gaji.setUangTransport(250000);
+        gaji.setUangTunjangan(300000);
+        gaji.setGajiPokok(4500000);
+        gaji.totalGaji(gaji.getUangTunjangan(),gaji.getUangTransport(),gaji.getGajiPokok());
+        gaji.tampilData(gaji.getNama(),gaji.getAlamat(),gaji.getUangTunjangan(),gaji.getUangTransport(),gaji.getGajiPokok(),gaji.totalGaji(gaji.getUangTunjangan(),gaji.getUangTransport(),gaji.getGajiPokok()));
+    }
+    
+}
